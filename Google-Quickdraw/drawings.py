2dbb230fc38e02274703ba8eaba0caba778cad7a
ArsalaBangash <arsalabangash@gmail.com>
2019-11-27 12:53:36 -0500

    Added files for calculator, dresser and house

--- Google-Quickdraw/drawings.py
+++ Google-Quickdraw/drawings.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
 import ndjson
 
 
@@ -31,7 +30,6 @@ data_load_state.text('✓ Data loaded!')
 # Notify the reader that the data was successfully loaded.
 st.write('Do people draw outside-in or inside-out?')
 st.write('Questions like this are interesting case studies in neural diversity!')
-st.write('By Arsala Bangash')
 
 
 @st.cache
@@ -42,28 +40,6 @@ def convert_data_to_dataframe(raw_data):
 if st.checkbox("Show Raw Data"):
     st.subheader("Raw Data")
     st.write(convert_data_to_dataframe(raw_drawings))
-
-st.write("-----------------------------------------------------------------------------")
-st.write("Studies have found that children with ASD tend to draw 'fragmented' pictures (disjointed appearance in the drawing) when compared to TD children. (Fein et al. 1990) (Booth et al. 2003)")
-st.write("This could be linked with the total number of strokes used (ex. drawing a window by drawing two intersecting lines versus drawing four separate lines)")
-st.write('Looking at the distribution of strokes used to complete the drawing:')
-total_strokes = (np.array([len(drawing['strokes'])
-                           for drawing in raw_drawings]))
-plt.hist(x=total_strokes, bins=num_drawings//5, color='blue',
-         edgecolor='black', alpha=0.7, rwidth=0.85, histtype='stepfilled')
-plt.title('Histogram of Strokes Used')
-plt.xlabel('Number of Strokes')
-plt.ylabel('Frequency')
-
-plt.show()
-st.pyplot()
-
-st.write("Potential follow-up questions:")
-st.write("1) For people who use more strokes than the average, are they drawing in a similar pattern (ie. inside-out vs outside-in)?")
-st.write("2) Is there anything similar about their location (ex. people in close locations have different interpretation of the same word and require more strokes)? ")
-
-
-st.write("-----------------------------------------------------------------------------------------------")
 
 st.header('Plots')
 st.write("Plotting each drawing's strokes show us how they evolve over time.")
